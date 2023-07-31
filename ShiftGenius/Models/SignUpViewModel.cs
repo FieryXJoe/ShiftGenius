19e02474d58080e95c8e4c058424e258129e14c8
Michael Lopez <malopez1@email.neit.edu>
2023-07-31 12:48:54 -0400

    Added Sign-Up pages, Added CSS changes to _layout.cshtml to reflect Prototype.

---
+++ ShiftGenius/Models/SignUpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShiftGenius.Models
+{
+    // SignUpViewModel.cs
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage = "Please enter your email.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
