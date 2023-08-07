7a109534e020716e18a82ada534b4e6647a6c3de
Michael Lopez <malopez1@email.neit.edu>
2023-08-07 13:35:48 -0400

    Re-Cloned. Added changes to appropriate files. employeeHome is now Index in Employee folder. Added employeeTimeOffRequest view and associated files. These files are currently skeletons, no logic included yet.

---
+++ ShiftGenius/Models/EmployeeTimeOffRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ShiftGenius.Models
+{
+    public class EmployeeTimeOffRequestViewModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+    }
+
+}
