2627f7a12dabb34227bcdf00d33c32820f7f6b86
Michael Lopez <malopez1@email.neit.edu>
2023-08-14 11:45:20 -0400

    Fixing build errors for TimeOff, still need to address AvailabilityRequest

--- ShiftGenius/Models/EmployeeAvailChangeModel.cs
+++ ShiftGenius/Models/EmployeeAvailChangeModel.cs
@@ -1,16 +1,14 @@
-using System.Collections.Generic;
-
 namespace ShiftGenius.Models
 {
 	public class EmployeeAvailChangeModel
 	{
-		public Dictionary<string, DayAvailability> Availability { get; set; }
+		public Dictionary<string, DayAvailability>? Availability { get; set; }
 	}
 
 	public class DayAvailability
 	{
 		public bool Enabled { get; set; }
-		public string StartTime { get; set; }
-		public string EndTime { get; set; }
+		public string? StartTime { get; set; }
+		public string? EndTime { get; set; }
 	}
 }
