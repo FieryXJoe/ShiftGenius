b757736b683ef42c04c2233e6b09db6a078fcef6
Michael Lopez <malopez1@email.neit.edu>
2023-08-18 10:43:55 -0400

    created ManagerTimeOffRequests skeleton

--- ShiftGenius/Controllers/ManagerController.cs
+++ ShiftGenius/Controllers/ManagerController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ShiftGenius.Models;
+using System.Collections.Generic;
 
 namespace ShiftGenius.Controllers
 {
@@ -9,6 +11,27 @@ namespace ShiftGenius.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize(Policy = "IsManager")]
+        public IActionResult ManagerTimeOffRequests()
+        {
+            var viewModel = new ManagerTimeOffRequestsViewModel
+            {
+                TimeOffRequests = GetMockTimeOffRequests() // Replace with your logic to retrieve time-off requests
+            };
+
+            return View(viewModel);
+        }
+
+        // This is just a mock function to generate sample data, replace it with actual data retrieval logic
+        private List<TimeOffRequest> GetMockTimeOffRequests()
+        {
+            return new List<TimeOffRequest>
+            {
+                new TimeOffRequest { EmployeeName = "Joe", StartDate = "2023-08-15", EndDate = "2023-08-17", TypeOfTimeOff = "Vacation" },
+                new TimeOffRequest { EmployeeName = "Mike", StartDate = "2023-08-18", EndDate = "2023-08-20", TypeOfTimeOff = "Sick Leave" }
+            };
         }
     }
 }
